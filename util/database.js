87be7d3d474aa939940bad637eeeb485b9aa5cff
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-01-27 12:45:05 +0100

    Using the database connection

--- util/database.js
+++ util/database.js
@@ -1,14 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
     MongoClient.connect(
-        'mongodb+srv://xxxxxxxxxx@cluster0-flk3y.mongodb.net/test?retryWrites=true')
+        'mongodb+srv://xxxxxxx@cluster0-flk3y.mongodb.net/shop?retryWrites=true')
         .then(client => {
             console.log('connected!');
-            callback(client)
+            _db = client.db();
+            callback()
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+           console.log(err)
+            throw err;
+        });
+};
+
+const getDb = () => {
+   if(_db) {
+      return _db;
+   }
+   throw 'No database found!';
 }
 
-module.exports = mongoConnect;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
