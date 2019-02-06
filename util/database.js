12c8f9fa6ef3daf87bbe6cb897cc6c13fb696b3a
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-02-06 20:33:36 +0100

    displaying the cart items and  deleting cart item

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = (callback) => {
     MongoClient.connect(
-        'mongodb+srv://xxxxxxx@cluster0-flk3y.mongodb.net/shop?retryWrites=true', { useNewUrlParser: true })
+        'mongodb+srv://Michael:b8sSmwk7I7wz7hpf@cluster0-flk3y.mongodb.net/shop?retryWrites=true', { useNewUrlParser: true })
         .then(client => {
             console.log('connected!');
             _db = client.db();
